4446cad991e95628840208752b4bbd8ffcf7233a
Jack Zhou <xza194@sfu.ca>
2018-12-23 15:32:41 -0800

    Update perl.lua
    noise function a *bit* closer to being correct now
    
    still need to work on when particles reach edge of a box, 
    current behaviour just draws an ugly line
    
    also need to look into a coloring function

--- perl.lua
+++ perl.lua
@@ -1,5 +1,7 @@
 require("linalg")
 
+-- idea: each star gets its own internal counter, so new stars will be dark instead of very white
+
 local canonicals =    {
                         {0,-1},
                         {math.sqrt(2)/2,-math.sqrt(2)/2},
@@ -14,35 +16,31 @@ local canonicals =    {
 local perl = {}
 
 perl.xdim,perl.ydim = 0,0
-perl.xscale,perl.yscale = 1200,600
-perl.boxsizex,perl.boxsizey = 0,0
+perl.xscale,perl.yscale = 600,600 -- needs to be divisors of 1200 and 600 lol
 perl.meteor_n = 1000
 perl.flow_speed = 5
 perl.internal_counter = 0
 
-function perl:getDims(w,h)
-        perl.xdim, perl.ydim = w, h
-        perl.boxsizex = perl.xdim/perl.xscale
-        perl.boxsizey = perl.ydim/perl.yscale
-end
+function perl:getDims(w,h) perl.xdim, perl.ydim = w, h end
 
 function perl:init()
         perl.grid = getmat( math.floor(perl.xdim/perl.xscale) + 1, math.floor(perl.ydim/perl.yscale) + 1 )
         perl:reset()
-
         perl.meteors = getmat(perl.meteor_n,1)
-        for i = 1,#perl.meteors do
-                perl.meteors[i] = {}
-                perl.meteors[i].x,perl.meteors[i].y = math.random()*perl.xdim,math.random()*perl.ydim
-                perl.meteors[i].xp,perl.meteors[i].yp = perl.meteors[i].x,perl.meteors[i].y
-        end
-        
+        for i = 1,#perl.meteors do perl.meteors[i] = perl:newStar() end
+end
+
+function perl:newStar()
+        local new_star = {}
+        new_star.x, new_star.y = math.random()*perl.xdim,math.random()*perl.ydim
+        new_star.xp, new_star.yp = new_star.x, new_star.y
+        return new_star
 end
 
 function perl:distributeGradients()
         for i = 1,#perl.grid do
                 for j = 1,#perl.grid[i] do
-                        -- METHOD ONE: by canonical directions
+                        -- -- METHOD ONE: by canonical directions
                         -- local pick = math.random(1,8)
                         -- perl.grid[i][j] = canonicals[pick]
 
@@ -52,25 +50,41 @@ function perl:distributeGradients()
         end
 end
 
--- PRECONDITION: x,y has to be within the screen!
+-- what if x,y are right on the boundaries?m
 function perl:getNoise(x,y)
         
+        -- grab indices of the nearest floored noise vector cell
         local heads_x, heads_y = math.floor(x/perl.xscale), math.floor(y/perl.yscale)
-        local tails_x, tails_y = x - heads_x*perl.xscale, y - heads_y*perl.yscale
+        
+        -- x,y proportions away from the nearest floored noise vector cell
+        local tails_x = perl:ease((x - heads_x*perl.xscale)/perl.xscale)
+        local tails_y = perl:ease((y - heads_y*perl.yscale)/perl.yscale)
 
-        local A = normalize({-tails_x,-tails_y})
-        local B = normalize({perl.boxsizex-tails_x,-tails_y})
-        local C = normalize({-tails_x,perl.boxsizey-tails_y})
-        local D = normalize({perl.boxsizex-tails_x,perl.boxsizey-tails_y})
-
+        local A = normalize({tails_x,tails_y})
+        local B = normalize({tails_x - 1, tails_y})
+        local C = normalize({tails_x,tails_y - 1})
+        local D = normalize({tails_x - 1, tails_y - 1})
+        
         local del_A = dot(A, perl.grid[heads_x+1][heads_y+1])
         local del_B = dot(B, perl.grid[heads_x+2][heads_y+1])
         local del_C = dot(C, perl.grid[heads_x+1][heads_y+2])
         local del_D = dot(D, perl.grid[heads_x+2][heads_y+2])
-
-        local interpolated = (del_A+del_B+del_C+del_D)/4
+        
+        -- bilinear interpolation
+        x1 =                    perl:lerp(del_A,del_B,tails_x/perl.xscale)
+        x2 =                    perl:lerp(del_C,del_D,tails_x/perl.xscale)
+        local interpolated =    perl:lerp(x1,x2,tails_y/perl.yscale)
+        
+        -- naive marginalization
+        -- local interpolated = (del_A+del_B+del_C+del_D)/4
 
         return interpolated
+end
+
+function perl:lerp(a,b,t) return a * (1-t) + b * t end
+
+function perl:ease(t)
+        return (t * t * t * (t * (t * 6 - 15) + 10))
 end
 
 function perl:keypressed(key,scancode,isrepeat)
@@ -87,58 +101,63 @@ function perl:reset()
         perl:distributeGradients()
         scene = love.graphics.newCanvas(perl.xdim,perl.ydim)
         perl.internal_counter = 0
+        perl.meteors = getmat(perl.meteor_n,1)
+        for i = 1,#perl.meteors do perl.meteors[i] = perl:newStar() end
 end
 
 function perl:draw()
-
         love.graphics.setColor(1,1,1,1)
         love.graphics.draw(scene)
 
+        -- debugging color circle
+        -- local current = perl:getColor(perl.internal_counter)
+        -- love.graphics.setColor(current[1],current[2],current[3],1)
+        -- love.graphics.circle("fill",0,0,50)
 end
 
 function perl:update(dt,frames)
 
-        if (perl.internal_counter % 30 == 0) then
-                perl.meteor_n = perl.meteor_n + 1
-                perl.meteors[perl.meteor_n] = {}
-                perl.meteors[perl.meteor_n].x,perl.meteors[perl.meteor_n].y = math.random()*perl.xdim,math.random()*perl.ydim
-                perl.meteors[perl.meteor_n].xp,perl.meteors[perl.meteor_n].yp = perl.meteors[perl.meteor_n].x,perl.meteors[perl.meteor_n].y
-        end
-
         for i,_ in pairs(perl.meteors) do
-                local turn = perl:getNoise(perl.meteors[i].x,perl.meteors[i].y)*2*math.pi -- radians
-                perl.meteors[i].xp = perl.meteors[i].x
-                perl.meteors[i].yp = perl.meteors[i].y
-
-                perl.meteors[i].x = perl.meteors[i].xp + perl.flow_speed*(math.cos(turn))
-                perl.meteors[i].y = perl.meteors[i].yp + perl.flow_speed*(math.sin(turn))
+                local step_direction = perl:getNoise(perl.meteors[i].x,perl.meteors[i].y)*2*math.pi
+                -- if (step_direction > 2*math.pi) then print("wtf") end
+                perl.meteors[i].xp, perl.meteors[i].yp = perl.meteors[i].x, perl.meteors[i].y
+                perl.meteors[i].x = perl.meteors[i].xp + perl.flow_speed * math.cos(step_direction)
+                perl.meteors[i].y = perl.meteors[i].yp + perl.flow_speed * math.sin(step_direction)
                 if (    (perl.meteors[i].x < 0) or (perl.meteors[i].x > perl.xdim) or 
                         (perl.meteors[i].y < 0) or (perl.meteors[i].y > perl.ydim)
-                ) then
-                        perl.meteors[i] = {}
-                        perl.meteors[i].x,perl.meteors[i].y = math.random()*perl.xdim,math.random()*perl.ydim
-                        perl.meteors[i].xp,perl.meteors[i].yp = perl.meteors[i].x,perl.meteors[i].y
-                end
+                ) then perl.meteors[i] = perl:newStar() end
         end
 
         love.graphics.setCanvas(scene)
-                love.graphics.setColor(perl.internal_counter/512,perl.internal_counter/32,perl.internal_counter,0.1)
-                for _,star in pairs(perl.meteors) do
-                        love.graphics.line(star.x,star.y,star.xp,star.yp)
-                end
+                local color = perl:getColor(perl.internal_counter)
+                love.graphics.setColor(color[1],color[2],color[3],0.1)
+                for _,star in pairs(perl.meteors) do love.graphics.line(star.x,star.y,star.xp,star.yp) end
         love.graphics.setCanvas()
 
         perl.internal_counter = perl.internal_counter + 1
 end
 
-function perl:getRandomUnit()
-        local vec = {math.random()*(math.random()-0.5),math.random()*(math.random()-0.5)}
-        
-        for i in ipairs(vec) do
-                vec[i] = vec[i]/norm(vec)
-        end
+-- we need a spicy coloring function
+function perl:getColor(frame)
 
-        return vec
+        local t = frame/10
+
+        -- -- original
+        local r = frame/1000
+        local g = frame/100
+        local b = frame/10
+
+        -- -- this thing gives me the pumping sensation
+        -- local r = (math.sin(t)+1)/2
+        -- local g = (math.cos(t)+1)/2
+        -- local b = 0.1
+
+        return {r,g,b}
+end
+
+function perl:getRandomUnit()
+        local direction = math.random()*2*math.pi
+        return {math.cos(direction), math.sin(direction)}
 end
 
 return perl
